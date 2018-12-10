47544977f4ed45619f41c002ddc03e5bf5753d7a
Omkar0532 <37664724+Omkar0532@users.noreply.github.com>
2018-12-10 17:13:50 +0530

    First Project
    Extracting data from excel and creating excel file with given template

---
+++ KPI_CFB Tool/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace KPI_CFB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lbl_infile = new System.Windows.Forms.Label();
+            this.tbXlPath = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblWait = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(506, 20);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl_infile
+            // 
+            this.lbl_infile.AutoSize = true;
+            this.lbl_infile.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_infile.Location = new System.Drawing.Point(7, 24);
+            this.lbl_infile.Name = "lbl_infile";
+            this.lbl_infile.Size = new System.Drawing.Size(144, 14);
+            this.lbl_infile.TabIndex = 1;
+            this.lbl_infile.Text = "Select Input Excel File";
+            // 
+            // tbXlPath
+            // 
+            this.tbXlPath.Location = new System.Drawing.Point(156, 21);
+            this.tbXlPath.Name = "tbXlPath";
+            this.tbXlPath.Size = new System.Drawing.Size(346, 22);
+            this.tbXlPath.TabIndex = 2;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRun.Location = new System.Drawing.Point(429, 65);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 34);
+            this.btnRun.TabIndex = 3;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btn_run_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(507, 65);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 34);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btn_cncl_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnBrowse);
+            this.groupBox1.Controls.Add(this.lbl_infile);
+            this.groupBox1.Controls.Add(this.tbXlPath);
+            this.groupBox1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(1, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(586, 53);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblWait
+            // 
+            this.lblWait.AutoSize = true;
+            this.lblWait.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWait.ForeColor = System.Drawing.Color.Blue;
+            this.lblWait.Location = new System.Drawing.Point(8, 75);
+            this.lblWait.Name = "lblWait";
+            this.lblWait.Size = new System.Drawing.Size(104, 14);
+            this.lblWait.TabIndex = 3;
+            this.lblWait.Text = "Please wait......";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 112);
+            this.Controls.Add(this.lblWait);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnRun);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CFB Creation Tool V1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lbl_infile;
+        private System.Windows.Forms.TextBox tbXlPath;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblWait;
+    }
+}
+
---
+++ KPI_CFB Tool/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace KPI_CFB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string ReplaceSpecialCharactersInFileName(string fname)
+        {
+            string[] schar = new string[]{"/", @"\", ":", "*", "?", "\"", "<", ">", "|"};
+            for (int i = 0; i < schar.Length; i++)
+            {
+                if(fname.Contains(schar[i]))
+                {
+                    fname = fname.Replace(schar[i], "_");
+                }
+            }
+            return fname;
+        }
+        private List<TrackerInfo> ReadExcelFile()
+        {
+            List<TrackerInfo> lstTrackerInfo = new List<TrackerInfo>();
+            try
+            {
+                
+                //Create COM Objects. Create a COM object for everything that is referenced
+                Excel.Application xlApp = new Excel.Application();
+                xlApp.Visible = false;
+                xlApp.DisplayAlerts = false;
+                Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(tbXlPath.Text.Trim(), Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+                Excel.Worksheet xlWorksheet = xlWorkbook.Worksheets[1];
+                //read the excel file
+                for (int nr = 2; nr <= xlWorksheet.UsedRange.Rows.Count + 5; nr++)
+                {
+                    TrackerInfo oTracker = new TrackerInfo();
+                    oTracker.m_sRFQNumber = GetCellValue(xlWorksheet, nr, 1);
+                    oTracker.m_sQuotationNumber = GetCellValue(xlWorksheet, nr, 2);
+                    oTracker.m_sDiehlResponsible = GetCellValue(xlWorksheet, nr, 3);
+                    oTracker.m_sProjectTitle = GetCellValue(xlWorksheet, nr, 4);
+                    oTracker.m_sOnsite = GetCellValue(xlWorksheet, nr, 5);
+                    oTracker.m_sOffshore = GetCellValue(xlWorksheet, nr, 6);
+                    oTracker.m_sSentOn = GetCellValue(xlWorksheet, nr, 7);
+                    string fname = GetCellValue(xlWorksheet, nr, 8);
+                    oTracker.m_sFileName = ReplaceSpecialCharactersInFileName(fname);
+                    oTracker.m_sFileLocation = GetCellValue(xlWorksheet, nr, 9);
+                    //end of reading
+                    if (oTracker.m_sRFQNumber.Length == 0) break;
+                    lstTrackerInfo.Add(oTracker);
+                }
+                // Close the excel.
+                xlWorkbook.Close(false, Type.Missing, Type.Missing);
+                Marshal.ReleaseComObject(xlWorksheet);
+                Marshal.ReleaseComObject(xlWorkbook);
+                xlApp.Quit();
+                Marshal.ReleaseComObject(xlApp);
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+            return lstTrackerInfo;
+        }
+        private bool WriteExcelFile(TrackerInfo oTracker)
+        {
+            try
+            {
+                string sTemplatePath = Application.StartupPath + "\\bin\\" + "KPI-CFB Template.xlsx";
+                string sNewFileName = oTracker.m_sFileLocation + "\\" + oTracker.m_sFileName + ".xlsx";
+                //check for folder exists
+                if (System.IO.Directory.Exists(oTracker.m_sFileLocation) && oTracker.m_sFileName.Length > 0)
+                {
+                    //Delete the file if exists
+                    int nResp = 0;
+                    if (System.IO.File.Exists(sNewFileName))
+                    {
+                        try
+                        {
+                            System.IO.File.Delete(sNewFileName);
+                        }
+                        catch
+                        {
+                            nResp = 1;
+                        }
+                    }
+                    //every thing is OK
+                    if (nResp == 0)
+                    {
+                        //copy the file
+                        System.IO.File.Copy(sTemplatePath, sNewFileName, true);
+                        //open excel instance
+                        Excel.Application xlApp = new Excel.Application();
+                        xlApp.DisplayAlerts = false;
+                        Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(sNewFileName, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+                        Excel.Worksheet xlWorksheet = xlWorkbook.Worksheets[1];
+                        //push the data
+                        xlWorksheet.get_Range("B3", "B3").Value2 = oTracker.m_sRFQNumber;
+                        xlWorksheet.get_Range("B4", "B4").Value2 = oTracker.m_sQuotationNumber;
+                        xlWorksheet.get_Range("B5", "B5").Value2 = oTracker.m_sDiehlResponsible;
+                        xlWorksheet.get_Range("B6", "B6").Value2 = oTracker.m_sProjectTitle;
+                        xlWorksheet.get_Range("C11", "C11").Value2 = oTracker.m_sOnsite;
+                        xlWorksheet.get_Range("D11", "D11").Value2 = oTracker.m_sOffshore;
+                        xlWorksheet.get_Range("B26", "B26").Value2 = oTracker.m_sSentOn;
+                        //save & close
+                        xlWorkbook.Save();
+                        xlWorkbook.Close(true);
+                        //Memory release
+                        Marshal.ReleaseComObject(xlWorksheet);
+                        Marshal.ReleaseComObject(xlWorkbook);
+                        xlApp.Quit();
+                        Marshal.ReleaseComObject(xlApp);
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+            return false;
+        }
+
+        private string GetCellValue(Excel._Worksheet xlWorksheet, int nRw, int nCl)
+        {
+            string sCellValue = "";
+            try
+            {
+                if (xlWorksheet.Cells[nRw, nCl] != null && xlWorksheet.Cells[nRw, nCl].Value2 != null)
+                {
+                    sCellValue = xlWorksheet.Cells[nRw, nCl].Value2.ToString();
+                }
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+            return sCellValue.Trim();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string fpath = "";
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "Excel File Dialog";
+            fdlg.InitialDirectory = @"c:\";
+            fdlg.Filter = "(*.xlsx*)|*.xlsx*";
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                tbXlPath.Text = fdlg.FileName;
+                //fpath = fdlg.FileName;
+            }
+        }
+
+        private void btn_cncl_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_run_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (System.IO.File.Exists(tbXlPath.Text.Trim()))
+                {
+                    //please wait label
+                    lblWait.Visible = true;
+                    lblWait.Refresh();
+                    this.Refresh();
+                    //read excel file
+                    List<TrackerInfo> lstTrackerInfo = ReadExcelFile();
+                    //write the data
+                    string sResp = "";
+                    for(int i=0;i<lstTrackerInfo.Count;i++)
+                    //foreach (TrackerInfo oTracker in lstTrackerInfo)
+                    {
+                        bool bResp = WriteExcelFile(lstTrackerInfo[i]);
+                        if (bResp == false)
+                        {
+                            sResp = sResp + lstTrackerInfo[i].m_sRFQNumber + "\n";
+                        }
+                    }
+                    //Final statement
+                    if (sResp.Trim().Length > 0)
+                    {
+                        MessageBox.Show("Failed to update the following RFQ's" + "\n\n" + sResp);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Successfully updated all the data");
+                    }
+                    this.Close();
+                }
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                lblWait.Visible = false;
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+        }
+    }
+    public class TrackerInfo
+    {
+        public string m_sRFQNumber = "";
+        public string m_sQuotationNumber = "";
+        public string m_sDiehlResponsible = "";
+        public string m_sProjectTitle = "";
+        public string m_sOnsite = "";
+        public string m_sOffshore = "";
+        public string m_sSentOn = "";
+        public string m_sFileName = "";
+        public string m_sFileLocation = "";
+        public bool m_bIsUpdated = false;
+    }
+}
